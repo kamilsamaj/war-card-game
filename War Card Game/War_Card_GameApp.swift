84cc9828214a89f980ce67169dbb95cc51e8a024
Kamil Samaj <kamil.samaj@accolade.com>
2024-01-21 15:29:35 +0100

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Kamil Samaj on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
